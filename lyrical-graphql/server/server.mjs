cd4d311d73fef5f5e5afba547877e3249fb7b6b9
fahmi ahmad <fahmiahmad@tutanota.com>
2022-03-15 21:26:07 +0700

    cors

--- lyrical-graphql/server/server.mjs
+++ lyrical-graphql/server/server.mjs
@@ -1,11 +1,21 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import "./models/index.mjs";
 import schema from "./schema/schema.mjs";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 
 const MONGO_URI = "mongodb://localhost/lyricaldb";
 if (!MONGO_URI) {
