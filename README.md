e16007f880b5206ab0e4c018392e3b003aede91c
fahmi ahmad <fahmiahmad@tutanota.com>
2022-03-15 21:17:16 +0700

    initialize apolloprovider

--- README.md
+++ README.md
@@ -7,3 +7,38 @@ changes I made:
 1. `.mjs`
 2. some API changes in graphql
 3. mongodb in local container
+
+## mongodb
+
+```shell
+docker run --name fdm-mongo -p 27017:27017 -d mongo
+```
+
+## lyrical-graphql `localhost:4000/graphql`
+
+```graphql
+mutation {
+  addSong(title: "Cold Night") {
+    id
+  }
+}
+
+mutation {
+  addLyricToSong(
+    content: "oh my oh my it's a cold night"
+    songId: "62309b2577d41d1d22a115c8"
+  ) {
+    id
+  }
+}
+
+{
+  songs {
+    id
+    title
+    lyrics {
+      content
+    }
+  }
+}
+```
--- lyrical-graphql/client/src/index.js
+++ lyrical-graphql/client/src/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-client";
-import { ApolloProvider } from "react-apollo";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import "./style/style.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
