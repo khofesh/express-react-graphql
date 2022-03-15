7ee30412add4a60c2740f13c83b47fc79f3eb3ac
fahmi ahmad <fahmiahmad@tutanota.com>
2022-03-15 20:20:09 +0700

    start from scratch

--- lyrical-graphql/client/src/components/SongDetail.js
+++ lyrical-graphql/client/src/components/SongDetail.js
@@ -1,27 +1,9 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { Link } from 'react-router';
-import fetchSong from '../queries/fetchSong';
-import LyricCreate from './LyricCreate';
-import LyricList from './LyricList';
-
-class SongDetail extends Component {
-  render() {
-    const { song } = this.props.data;
-
-    if (!song) { return <div>Loading...</div>; }
-
-    return (
-      <div>
-        <Link to="/">Back</Link>
-        <h3>{song.title}</h3>
-        <LyricList lyrics={song.lyrics} />
-        <LyricCreate songId={this.props.params.id} />
-      </div>
-    );
-  }
+function SongDetail() {
+  return (
+    <div>
+      <p>song detail</p>
+    </div>
+  );
 }
 
-export default graphql(fetchSong, {
-  options: (props) => { return { variables: { id: props.params.id } } }
-})(SongDetail);
+export default SongDetail;
